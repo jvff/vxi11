c87b8455fbd97c80206aac39badb7d9b93d7467d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-29 19:34:05 +0000

    Implement DeviceReadParameters constructor
    - Device link ID and number of bytes to read are necessary parameters

--- src/device/device_read_parameters.rs
+++ src/device/device_read_parameters.rs
@@ -10,3 +10,16 @@ pub struct DeviceReadParameters {
     flags: DeviceFlags,
     termination_char: char,
 }
+
+impl DeviceReadParameters {
+    pub fn new(link_id: DeviceLink, bytes_requested: u32) -> Self {
+        DeviceReadParameters {
+            link_id,
+            bytes_requested,
+            io_timeout: 0,
+            lock_timeout: 0,
+            flags: DeviceFlags::default(),
+            termination_char: '\0',
+        }
+    }
+}
