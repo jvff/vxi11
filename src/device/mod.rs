339d31928d64d4419743ef8f6429a775b5690823
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-09-29 16:08:53 +0000

    Define initial CoreChannel program
    - For now it only has the create_channel procedure

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -2,3 +2,20 @@ mod device_error_code;
 mod device_link;
 mod create_link_parameters;
 mod create_link_response;
+
+pub use self::create_link_parameters::CreateLinkParameters;
+
+use self::create_link_response::CreateLinkResponse;
+
+onc_rpc! {
+    program(core::CoreChannel) {
+        id = 395_183;
+        version = 1;
+        connect = CoreChannelConnect;
+
+        procedures {
+            10 => create_link(link_parameters: CreateLinkParameters)
+                -> CreateLinkResponse => CreateLinkResult,
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
+#[macro_use]
+extern crate error_chain;
+#[macro_use]
+extern crate futures;
+#[macro_use]
+extern crate onc_rpc;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+extern crate tokio_core;
+extern crate tokio_proto;
+extern crate tokio_service;
 
-mod device;
+pub mod device;
